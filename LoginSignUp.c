b768a6c20f6204d3e74d656bc56851ef280d1f17
Rohith Pala <63358944+rohithpala@users.noreply.github.com>
2021-06-14 12:43:25 +0530

    Login Signup Code in C
    This code helps to get the functionality of login signup
    Main Features:
    1) Hiding Password - This Code Prints * instead of your password to maintain privacy
    2) Uses File Handling in C
    3) Program terminates if the given credentials are wrong, however you can change this functionality if you want

--- LoginSignUp.c
+++ LoginSignUp.c
@@ -4,71 +4,124 @@
 #include<windows.h>
 #include<conio.h>
 
-void SignUp_LogIn(char ch){
-     char username[20], password[20], scannedUsername[20], scannedPassword[20], c, found = '0';
+// Signup and Login Code
+char SignUp_LogIn(char ch){
+     const int maxPasswordLength = 16;
+     char username[20], password[maxPasswordLength+1], scannedUsername[20], scannedPassword[maxPasswordLength+1], c, found = '0';
      FILE *adminFile;
      int i = 0;
      if(ch == 'S'){
-          printf("Username: ");
-          scanf("%s", username);
-          adminFile = fopen("LogInSignUpDatabase.txt", "r");
-          while(fscanf(adminFile, "%s %s\n", scannedUsername, scannedPassword) != EOF){
-               if(strcmp(scannedUsername, username) == 0){
-                    found = '1';
-                    printf("Username already taken\nProgram Exiting...\n");
-                    Sleep(1000);
-                    exit(0);
+          do{
+               printf("Username: ");
+               scanf("%s", username);
+               adminFile = fopen("LogInSignUpDatabase.txt", "r");
+               while(fscanf(adminFile, "%s %s\n", scannedUsername, scannedPassword) != EOF){
+                    if(strcmp(scannedUsername, username) == 0){
+                         found = '1';
+                         printf("Username already taken\n");
+                         break;
+                    } else {
+                         found = '0';
+                    }
                }
-          }
+          } while(found != '0'); // Only comes out when the username is a different one
 
-          if(found == '0'){
-               printf("Set Password: ");
-               while(i < 20){
-                    password[i] = getch();
-                    c = password[i];
-                    if(c == 13) break; // 13 is the ASCII value of \n, this takes input untill the user clicks enter key
-                    else printf("*"); // If not enter key to hide password we print "*"
-                    i++;
+          printf("Set Password: ");
+          while(1){
+               ch = getch();
+               if(ch == 13){ // if enter key
+                    break;
+               } else if(ch == 32 || ch == 9){ // if space or tab key
+                    continue;
+               } else if(ch == 8){ // if backspace
+                    if(i > 0){
+                         i--;
+                         password[i] = '\0';
+                         printf("\b \b");
+                    }
+               } else{
+                    if(i < maxPasswordLength){
+                         password[i] = ch;
+                         i++;
+                         printf("*");
+                    }
                }
-               password[i] = '\0';
-               printf("\n");
           }
+          password[i] = '\0';
+          printf("\n");
 
           fclose(adminFile);
           adminFile = fopen("LogInSignUpDatabase.txt" , "a");
           fprintf(adminFile, "%s %s\n", username, password);
           fclose(adminFile);
           printf("Account Created Successfully\n");
+          return '1';
      }
 
      else{
           printf("Username: ");
           scanf("%s", username);
           printf("Password: ");
-          while(i < 20){
-               password[i] = getch();
-               c = password[i];
-               if(c == 13) break;
-               else printf("*");
-               i++;
+          while(1){
+               ch = getch();
+               if(ch == 13){ // if enter key
+                    break;
+               } else if(ch == 32 || ch == 9){ // if space or tab key
+                    continue;
+               } else if(ch == 8){ // if backspace
+                    if(i > 0){
+                         i--;
+                         password[i] = '\0';
+                         printf("\b \b");
+                    }
+               } else{
+                    if(i < maxPasswordLength){
+                         password[i] = ch;
+                         i++;
+                         printf("*");
+                    }
+               }
           }
           password[i] = '\0';
           printf("\n");
           adminFile = fopen("LogInSignUpDatabase.txt", "r");
           while(fscanf(adminFile, "%s %s\n", scannedUsername, scannedPassword) != EOF){
-               if((strcmp(scannedUsername, username)) == 0 && strcmp(scannedPassword, password) == 0)
-               {
+               if((strcmp(scannedUsername, username)) == 0 && strcmp(scannedPassword, password) == 0){
                     found = '1';
                     printf("Logged In Successfully\n");
-                    break;
+                    return '1';
                }
           }
           fclose(adminFile);
-          if(found == '0')
-          {
-               printf("No User with the Given Credentials\nProgram Exiting...\n");
-               Sleep(1000);
-               exit(0);
+          if(found == '0'){
+               printf("No User with the Given Credentials\n");
+               return '0';
           }
      }
+}
+
+char callSignupLogin(){
+     char SLChoice[7], ret;
+     printf("SignUp or LogIn: ");
+     scanf("%s",SLChoice);
+     if(strcmp(strlwr(SLChoice), "signup") == 0)
+          return SignUp_LogIn('S');
+     return SignUp_LogIn('L');
+}
+
+void main()
+{
+     char returned, choice;
+     do{
+          returned = callSignupLogin();
+          if(returned == '1'){
+               // Write your Code Here
+               printf("Welcome User!"); // Dummy Sentence
+          } else {
+               printf("Want to Try Again [Y/N]? ");
+               scanf(" %c", &choice); // If 'Y' the user will get a chance to again signup/login
+          }
+          if(choice == 'N' || choice == 'n')
+               printf("Have A Great Day Ahead! :)\n");
+     }while(choice == 'Y' || choice == 'y');
 }
